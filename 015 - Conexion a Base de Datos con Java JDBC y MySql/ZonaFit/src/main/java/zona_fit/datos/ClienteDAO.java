69820654c45c4ff5c28f98c4ea5534066ca9942e
Frostwreek92 <frostwreek@gmail.com>
2026-05-06 19:15:17 +0200

    Fin Seccion 015

---
+++ 015 - Conexion a Base de Datos con Java JDBC y MySql/ZonaFit/src/main/java/zona_fit/datos/ClienteDAO.java
@@ -0,0 +1,261 @@
+package zona_fit.datos;
+
+import zona_fit.conexion.Conexion;
+import zona_fit.dominio.Cliente;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
+import static zona_fit.conexion.Conexion.getConexion;
+
+public class ClienteDAO implements IClienteDAO {
+    @Override
+    public List<Cliente> listarClientes() {
+        List<Cliente> clientes = new ArrayList<>();
+        PreparedStatement ps;
+        ResultSet rs;
+        Connection con = getConexion();
+        var sql = "SELECT * FROM cliente ORDER BY id";
+        try {
+            ps = con.prepareStatement(sql);
+            rs = ps.executeQuery();
+            while(rs.next()) {
+                var cliente = new Cliente();
+                cliente.setId(rs.getInt("id"));
+                cliente.setNombre(rs.getString("nombre"));
+                cliente.setApellido(rs.getString("apellido"));
+                cliente.setMembresia(rs.getInt("membresia"));
+                clientes.add(cliente);
+            }
+        } catch (Exception e) {
+            System.out.println("Error al listar clientes: " + e.getMessage());
+        }
+        finally {
+            try {
+                con.close();
+            } catch (Exception e) {
+                System.out.println("Error al cerrar conexion: " + e.getMessage());
+            }
+        }
+        return clientes;
+    }
+
+    @Override
+    public boolean buscarClientePorId(Cliente cliente) {
+        PreparedStatement ps;
+        ResultSet rs;
+        Connection con = getConexion();
+        String sql = "SELECT * FROM cliente WHERE id = ?";
+        try {
+            ps = con.prepareStatement(sql);
+            ps.setInt(1, cliente.getId());
+            rs = ps.executeQuery();
+            if (rs.next()) {
+                cliente.setNombre(rs.getString("nombre"));
+                cliente.setApellido(rs.getString("apellido"));
+                cliente.setMembresia(rs.getInt("membresia"));
+                return true;
+            }
+        } catch (Exception e) {
+            System.out.println("Error al recuperar cliente por ID: " + e.getMessage());
+        }
+        finally {
+            try {
+                con.close();
+            } catch (Exception e) {
+                System.out.println("Error al cerra conexion: " + e.getMessage());
+            }
+        }
+        return false;
+    }
+
+    @Override
+    public boolean agregarCliente(Cliente cliente) {
+        PreparedStatement ps;
+        Connection con = getConexion();
+        String sql = "INSERT INTO cliente(nombre, apellido, membresia)"
+                + " " + "VALUES(?, ?, ?)";
+        try {
+            ps = con.prepareStatement(sql);
+            ps.setString(1, cliente.getNombre());
+            ps.setString(2, cliente.getApellido());
+            ps.setInt(3, cliente.getMembresia());
+            ps.execute();
+            return true;
+        } catch (Exception e) {
+            System.out.println("Error al agregar cliente: " + e.getMessage());
+        }
+        finally {
+            try {
+                con.close();
+            } catch (Exception e) {
+                System.out.println("Error al cerrar conexion: " + e.getMessage());
+            }
+        }
+        return false;
+    }
+
+    @Override
+    public boolean modificarCliente(Cliente cliente) {
+        PreparedStatement ps;
+        Connection con = getConexion();
+        String sql = "UPDATE cliente SET nombre=?, apellido=?, membresia=?"
+                + " " + "WHERE id = ?";
+        try {
+            ps = con.prepareStatement(sql);
+            ps.setString(1, cliente.getNombre());
+            ps.setString(2, cliente.getApellido());
+            ps.setInt(3, cliente.getMembresia());
+            ps.setInt(4, cliente.getId());
+            ps.execute();
+            return true;
+        } catch (Exception e) {
+            System.out.println("Error al modificar cliente: " + e.getMessage());
+        }
+        finally {
+            try {
+                con.close();
+            } catch (Exception e) {
+                System.out.println("Error al cerrar conexion: " + e.getMessage());
+            }
+        }
+        return false;
+    }
+
+    @Override
+    public boolean eliminarCliente(Cliente cliente) {
+        PreparedStatement ps;
+        Connection con = getConexion();
+        String sql = "DELETE FROM cliente WHERE id = ?";
+        try {
+            ps = con.prepareStatement(sql);
+            ps.setInt(1, cliente.getId());
+            ps.execute();
+            return true;
+        } catch (Exception e) {
+            System.out.println("Error al eliminar cliente: " + e.getMessage());
+        }
+        finally {
+            try {
+                con.close();
+            } catch (Exception e) {
+                System.out.println("Error al cerrar conexion: " + e.getMessage());
+            }
+        }
+        return false;
+    }
+
+
+    static void listar (IClienteDAO clienteDao) {
+        // Listar clientes
+        System.out.println("*** Listar clientes ***");
+        /* Aquí se podría usar un var, pero para cumplir con los contratos creados, se utiliza la interfaz
+         * Así luego, puede reutilizarse para el resto de metodos que se han creado */
+        var clientes = clienteDao.listarClientes();
+        clientes.forEach(System.out::println);
+        System.out.println(); // Salto de linea vacio
+    }
+    static void buscarPorId(IClienteDAO clienteDao, Scanner scanner) {
+        // Buscar por ID
+        System.out.println("*** Buscar cliente por ID ***");
+        System.out.print("Introduce ID del cliente: ");
+        var idCliente = new Cliente(scanner.nextInt());
+        System.out.println("Cliente antes de la busqueda: " + idCliente);
+        var encontrado = clienteDao.buscarClientePorId(idCliente);
+        if (encontrado) {
+            System.out.println("Cliente encontrado: " + idCliente);
+        } else {
+            System.out.println("No se encontro registro: " + idCliente.getId());
+        }
+        System.out.println();
+    }
+    static void agregar(IClienteDAO clienteDao, Scanner scanner) {
+        // Agregar cliente
+        System.out.println("*** Agregar cliente ***");
+        System.out.print("Introduce nombre: ");
+        var nombre = scanner.nextLine();
+        System.out.print("Introduce apellido: ");
+        var apellido = scanner.nextLine();
+        System.out.print("Introduce membresia: ");
+        var membresia = scanner.nextInt();
+
+        var nuevoCliente = new Cliente(nombre, apellido, membresia);
+        var agregado = clienteDao.agregarCliente(nuevoCliente);
+        if (agregado) {
+            System.out.println("Cliente agregado: " + nuevoCliente);
+        } else {
+            System.out.println("No se agrego el cliente: " + nuevoCliente);
+        }
+        System.out.println();
+    }
+    static void modificar(IClienteDAO clienteDao, Scanner scanner) {
+        // Modificar cliente
+        System.out.println("*** Modificar cliente ***");
+        System.out.print("Introduce ID del cliente a modificar: ");
+        var idCliente = scanner.nextInt();
+        System.out.print("Introduce nombre: ");
+        var nombre = scanner.nextLine();
+        System.out.print("Introduce apellido: ");
+        var apellido = scanner.nextLine();
+        System.out.print("Introduce membresia: ");
+        var membresia = scanner.nextInt();
+
+        var modificarCliente = new Cliente(idCliente, nombre, apellido, membresia);
+        var modificado = clienteDao.modificarCliente(modificarCliente);
+        if (modificado) {
+            System.out.println("Cliente modificado: " + modificarCliente);
+        } else {
+            System.out.println("No se modifico el cliente: " + modificarCliente);
+        }
+        System.out.println();
+    }
+    static void eliminar(IClienteDAO clienteDao, Scanner scanner) {
+        // Eliminar cliente
+        System.out.println("*** Eliminar cliente ***");
+        System.out.print("Selecciona el ID a eliminar: ");
+        var idClienteEliminar = scanner.nextInt();
+        var clienteEliminar = new Cliente(idClienteEliminar);
+        var eliminado = clienteDao.eliminarCliente(clienteEliminar);
+        if (eliminado) {
+            System.out.println("Cliente eliminado: " + clienteEliminar);
+        } else {
+            System.out.println("No se elimino cliente: " + clienteEliminar);
+        }
+    }
+
+    /* Aquí se ejecutaria el codigo creado por mi
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        IClienteDAO clienteDao = new ClienteDAO();
+        var salir = false;
+        int opcion;
+
+        while (!salir) {
+            System.out.println("1.Listar"
+            + "\n2.Buscar por ID"
+            + "\n3.Agregar"
+            + "\n4.Modificar"
+            + "\n5.Eliminar"
+            + "\n0. Salir");
+            System.out.print("Elige una opcion: ");
+            opcion = scanner.nextInt();
+            switch (opcion) {
+                case 1 -> listar(clienteDao);
+                case 2 -> buscarPorId(clienteDao, scanner);
+                case 3 -> agregar(clienteDao, scanner);
+                case 4 -> modificar(clienteDao, scanner);
+                case 5 -> eliminar(clienteDao, scanner);
+                case 0 -> {
+                        System.out.println("Saliendo...");
+                        salir = true;
+                }
+                default -> System.out.println("Escoge una de las opciones anteriores.");
+            }
+        }
+    }
+    */
+}
