fff7a925c0d10a341f9670fee22d32729cb137f1
Frostwreek92 <frostwreek@gmail.com>
2026-05-07 12:57:49 +0200

    Fin Seccion 014

---
+++ 014 - Manejo de Archivos en Java/Ejemplos/14-08-00-ConstructorServicioArchivos-UJB/maquina_snacks_archivos/servicio/ServicioSnacksArchivos.java
@@ -0,0 +1,53 @@
+package maquina_snacks_archivos.servicio;
+
+import maquina_snacks_archivos.dominio.Snack;
+
+import java.io.File;
+import java.io.FileWriter;
+import java.io.PrintWriter;
+import java.util.ArrayList;
+import java.util.List;
+
+public class ServicioSnacksArchivos implements IServicioSnacks{
+    private final String NOMBRE_ARCHIVO = "snacks.txt";
+    // Crear la lista de snacks
+    private List<Snack> snacks = new ArrayList<>();
+
+    // Constructor clase
+    public ServicioSnacksArchivos(){
+        //Creamos el archivo si no existe
+        var archivo = new File(NOMBRE_ARCHIVO);
+        var existe = false;
+        try{
+            existe = archivo.exists();
+            if(existe){
+                //this.snacks = obtenerSnacks();
+            }
+            else{ // Creamos el archivo
+                var salida = new PrintWriter(new FileWriter(archivo));
+                salida.close();//Guarda el archivo en disco
+                System.out.println("Se ha creado el archivo");
+            }
+        }catch(Exception e){
+            System.out.println("Error al crear el archivo: " + e.getMessage());
+        }
+        // Si no existe, cargamos algunos snacks iniciales
+        if(!existe)
+            cargarSnacksIniciales();
+    }
+
+    @Override
+    public void agregarSnack(Snack snack) {
+
+    }
+
+    @Override
+    public void mostrarSnacks() {
+
+    }
+
+    @Override
+    public List<Snack> getSnacks() {
+        return null;
+    }
+}
