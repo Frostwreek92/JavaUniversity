7750e615a998d5cc367734c09c59e26570a3a179
Frostwreek92 <frostwreek@gmail.com>
2026-05-13 00:30:09 +0200

    Inicio Seccion 019

---
+++ 019 - Sistema de Tareas con JavaFX y SpringBoot/Ejemplos/SistemaTareas/src/main/java/gm/tareas/TareasApplication.java
@@ -0,0 +1,16 @@
+package gm.tareas;
+
+import gm.tareas.presentacion.SistemaTareasFX;
+import javafx.application.Application;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TareasApplication {
+
+	public static void main(String[] args) {
+		//SpringApplication.run(SistemaEstudiantesApplication.class, args);
+		Application.launch(SistemaTareasFX.class, args);
+	}
+
+
+}
